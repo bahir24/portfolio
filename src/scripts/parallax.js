d6706733cbf8747d373bbb420e9aeafb4963f527
Mikhail_Bakhirev <7074033@gmail.com>
2019-04-19 03:36:26 +0300

    section_skills

---
+++ src/scripts/parallax.js
@@ -0,0 +1,21 @@
+
+const parallax = document.querySelector(".hero");
+const layers = parallax.children;
+
+function move(wScroll) {
+
+  Array.from(layers).forEach(layer => {
+
+    const lspeed = layer.dataset.speed;
+    const strafe = lspeed * wScroll / 60;
+
+    layer.style.transform = `translateY(-${strafe}%)`
+
+  });
+}
+
+window.addEventListener("scroll", function () {
+  const wScroll = window.pageYOffset;
+  move(wScroll);
+
+});
