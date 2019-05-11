27132330251902b70f05e9fd1b1b4edbbd238633
Mihail_Bahirev <7074033@gmail.com>
2019-05-11 22:29:21 +0300

    media

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,17 +1,18 @@
-
 const parallax = document.querySelector(".parallax");
 const layers = parallax.children;
-
+const last = parallax.lastElementChild;
+const grow = last.lastElementChild;
+  
 function move(wScroll) {
-
+  
   Array.from(layers).forEach(layer => {
-
     const lspeed = layer.dataset.speed;
     const strafe = lspeed * wScroll / 60;
-
-    layer.style.transform = `translateY(-${strafe}%)`
-
+    layer.style.transform = `translateY(-${strafe}%)`;
+    grow.style.height = 4 + strafe + "%";
+    
   });
+  
 }
 
 window.addEventListener("scroll", function () {
